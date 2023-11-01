c263d3242e2124adf035281d96a5ea6618707eb9
demiapollo <apollodemirel67@gmail.com>
2023-10-31 19:45:59 -0700

    pong game completed

--- ball.py
+++ ball.py
@@ -8,9 +8,31 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.shapesize(stretch_wid=1.5, stretch_len=1.5)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
         
+    def change_direction_down(self):
+        self.setheading(270)
+        self.move()
 
+   
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed * 0.9
+
+    
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
